a7da7edfd546bcfb0a97258babed926c0d1f1667
praizoh <oremei.akande@gmail.com>
2022-07-27 16:35:47 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}", "public/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors:{
+          primary:'#ff6363',
+          seondary:{
+            100: '#E2E2D5',
+            200:'#888883',
+          }
+        }
+      },
+    },
+    plugins: [],
+  }
